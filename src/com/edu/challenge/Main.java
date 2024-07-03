0080431efcb5d07196f9faf0d825833c12a026ef
salvadorf8 <aztecsalvador@gmail.com>
2024-07-03 16:27:03 -0500

    initial checking (used Amend)

--- src/com/edu/challenge/Main.java
+++ src/com/edu/challenge/Main.java
@@ -21,7 +21,7 @@ public class Main {
 
         System.out.println("Matches");
         var matches = queryList
-                .getMatches("PercentComplete", "50")
+                .getMatches("PercentComplete", "100")
                 .getMatches("Course", "Python");
 
         matches.sort(new LPAStudentComparator());
